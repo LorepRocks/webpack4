3e1fcbb7c0e64438296a6809ea8549fe8cb6f9af
LorepRocks <lorenajrc@gmail.com>
2020-10-04 21:25:38 -0500

    cwebpack configuration for support url-loader & file-loader

--- url-loader/src/js/component/app.js
+++ url-loader/src/js/component/app.js
@@ -1,6 +1,9 @@
 import React, { useState } from 'react';
 import data from './data.json';
 import Loader from './loader';
+import logo from '../../images/platzi.png';
+import video from '../../video/que-es-core.mp4';
+
 console.log(data);
 function App() {
   const [loaderList, setLoaderList] = useState([]);
@@ -10,6 +13,10 @@ function App() {
   return (
     <div>
       <div>Hi Human! I'm React!</div>
+      <video src={video} width={360} controls poster={logo}></video>
+      <p>
+        <img src={logo} alt='' width={40} />
+      </p>
       <ul>
         {loaderList.map((item) => (
           <Loader {...item} key={item.id} />
